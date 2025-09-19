2bc9a4317efb409e690abcb081b873d17bcf16d6
Greg Griffin <greg.griffin2@gmail.com>
2025-09-19 00:06:33 -0400

    docs

--- cache.go
+++ cache.go
@@ -36,7 +36,13 @@ func GetSRGBCache() *RGBCache {
 	return sRGBCache
 }
 
-// RGBCache caches computed data given an RGBColor
+// RGBCache caches computed data given an RGBColor.
+// I added this because my TUI application renders markdown text with glamour (which calls funcs in this package)
+// many times per second over and over again. Since this is the main functionality of my TUI, I profiled this feature
+// and there were 3 funcs in termenv that were using much of the CPU time. Once I realized that my TUI would only ever
+// need a fixed number of terminal colors/styles (computed by this package every time glamour renders markdown), I figured
+// I'd create a cache for these. These caches (and one other perf tweak) led to almost a 2x reduction in CPU time for
+// the code-path I was targeting, and a 5x speedup in the direct callee of these termenv functions I modified.
 type RGBCache struct {
 	data sync.Map
 
@@ -46,7 +52,7 @@ type RGBCache struct {
 }
 
 type entry struct {
-	value      interface{}
+	value      interface{} // go 1.18 generics would be nice to have here!
 	lastAccess int64
 }
 
